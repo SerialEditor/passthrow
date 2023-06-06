fe5aa22826e6b607b0e5780e3f6cc1a9f8c602d1
SerialEditor <refservicesamara@yandex.ru>
2023-06-06 19:12:15 +0400

    feat: Изменено имя метода экспортируемого библиотекой объекта
    BREAKING CHANGE: Имя метода 'getUniqPositions' изменено на 'filterInterestingIndices' с целью более лаконично выразить сущность возвращаемого методом результата

--- passthrow.js
+++ passthrow.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-param-reassign */
 const passthrow = {
-  filterUniqIndices(assembledItems, selectedItems) {
+  filterInterestingIndices(assembledItems, selectedItems) {
     if (!Array.isArray(assembledItems)) assembledItems = Array.from(assembledItems);
     if (!Array.isArray(selectedItems)) selectedItems = Array.from(selectedItems);
     let sequenceIndex = 0;
